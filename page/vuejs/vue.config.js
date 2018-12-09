9931ae71cd57471a34e213677bfca76814bb3438
MongMX <mongmx@gmail.com>
2018-12-09 17:14:20 +0700

    clean

--- page/vuejs/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  outputDir: '../assets/dist',
-  assetsDir: undefined,
-  productionSourceMap: false,
-  baseUrl: 'assets/',
-  runtimeCompiler: undefined,
-  parallel: undefined,
-  css: undefined
-}
