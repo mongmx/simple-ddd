3b6ba7ae21eaffc13501974c76fbdddda705f685
MongMX <mongmx@gmail.com>
2018-12-27 22:44:32 +0700

    Update 20181123212610-create_products_table.sql

--- database/migrations/postgres/20181123212610-create_products_table.sql
+++ database/migrations/postgres/20181123212610-create_products_table.sql
@@ -4,13 +4,7 @@ create table products (
 	id bigserial,
 	supplier_id bigint,
 	name varchar not null,
-	unit json, --{kg,100g,g} {bag,pack,...}
-	price_1 numeric default 0,
-	price_2 numeric default 0,
-	price_3 numeric default 0,
-	time_to_sell_start timestamp,
-	time_to_sell_end timestamp,
-	all_day_to_sell boolean, -- or auto fill-in ui 
+	price numeric default 0, 
 	created_at timestamp default now(),
 	updated_at timestamp default now(),
 	deleted_at timestamp null,
